9f58a38c772249fefa69ba17c15c8bd646550179
negisaro <nesalaz56@gmail.com>
2020-03-26 17:45:07 -0500

    commit Inicial

---
+++ Datos_Por_Huella/app/src/main/java/giova/varios/datos_por_huella/HomeActivity.java
@@ -0,0 +1,16 @@
+package giova.varios.datos_por_huella;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
